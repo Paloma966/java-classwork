1ffc7044d2dea78a65fe49cedb2cea0979b28e23
paloma966 <xiejunbo2580@163.com>
2026-03-24 20:31:15 +0800

    chore: practice java

--- src/main/java/com/example/practice/Main.java
+++ src/main/java/com/example/practice/Main.java
@@ -1,34 +1,30 @@
 package 
 com.example.practice;
+
 public class Main {
     public static void main(String[] args) {
-        Person ming = new Person();
-        ming.setName("小明");
-        ming.setAge(12);
+        // TODO: 给Person增加构造方法:
+        Person ming = new Person("小明", 12);
+        System.out.println(ming.getName());
         System.out.println(ming.getAge());
     }
 }
 
 class Person {
     private String name;
+    private int age;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private int age;
-
     public int getAge() {
         return age;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    
 }
+
